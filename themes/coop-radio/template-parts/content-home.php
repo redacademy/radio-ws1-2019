f7d3bbbd166349747827b0f10beb826ed26fbd34
khs6226 <khsun6226@naver.com>
2019-08-27 19:38:55 -0700

    create front-page.php and add contents into the page.

---
+++ themes/coop-radio/template-parts/content-home.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @package coop-radio
+ */
+
+?>
+
+<section class='home-img' style='background-image:url(wp-content/themes/coop-radio/images/front-page-background.jpeg);'>
+	<div>
+        <h1 style='color:#fff;'>Program Name</h1>
+
+        <p style='color:#fff;'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Pariatur nulla dolor voluptatem suscipit? 
+        Numquam impedit labore sed amet ut aperiam, 
+        a eaque eum qui tempore ab accusantium adipisci deleniti quisquam.</p>
+
+        <p>
+            <a href="#program-details" style='color:#fff;'>View</a>
+        </p>
+        
+    </div>
+</section>

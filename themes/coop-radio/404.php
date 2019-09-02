2cf06064fa89fd5d8d54cfd37d2a2f789cca87da
Hongsoon Kim <khsun6226@naver.com>
2019-09-02 12:58:12 -0700

    add background image to 404page.

--- themes/coop-radio/404.php
+++ themes/coop-radio/404.php
@@ -9,10 +9,15 @@
 
 get_header(); ?>
 
-  <main>
-    <section>
-      <a href="<?php echo esc_url(home_url('/')); ?>">Back to Home</a>
-    </section>
+  <main class='not-found'>
+    <div class='front-container'>
+      <img src='<?php echo get_stylesheet_directory_uri().'/images/404.PNG'; ?>' alt="404 image">
+      <div>
+        <p>Oops!</p>
+        <p>Page not found</p>
+        <p><a href="<?php echo esc_url(home_url('/')); ?>">Back to Home</a></p>
+      </div>
+    </div>
   </main>
 
 <?php get_footer(); ?>

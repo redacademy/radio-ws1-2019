e4c4e4a09b70daff30866d56c85e6b5fd86e94b4
Hongsoon Kim <khsun6226@naver.com>
2019-09-02 12:59:55 -0700

    change the name for front-page.

--- themes/coop-radio/template-parts/content-front-page.php
+++ themes/coop-radio/template-parts/content-front-page.php
@@ -7,17 +7,17 @@
 
 ?>
 
-<section class='home-img' style='background-image:url(wp-content/themes/coop-radio/images/front-page-background.jpeg);'>
+<section class='home-image'>
 	<div>
-        <h1 style='color:#fff;'>Program Name</h1>
+        <h1>Program Name</h1>
 
-        <p style='color:#fff;'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
         Pariatur nulla dolor voluptatem suscipit? 
         Numquam impedit labore sed amet ut aperiam, 
         a eaque eum qui tempore ab accusantium adipisci deleniti quisquam.</p>
 
         <p>
-            <a href="#program-details" style='color:#fff;'>View</a>
+            <a href="#program-details">View</a>
         </p>
         
     </div>

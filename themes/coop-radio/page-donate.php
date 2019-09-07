9ef3023e135398eb961c347086dfd367244b283d
Graziela <grazims26@gmail.com>
2019-09-07 13:55:34 -0700

    create donate page

---
+++ themes/coop-radio/page-donate.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying all pages.
+ * Template Name: donate
+ *
+ * @package coop-radio
+ */
+
+get_header(); ?>
+
+  <main>
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+      <?php get_template_part( 'template-parts/content-page-donate' ); ?>
+
+    <?php endwhile; // End of the loop. ?>
+
+  </main>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>

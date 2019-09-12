f0375d3178ecd1ba587f6aec175a494de488012d
William L <w-lindvall@outlook.com>
2019-09-11 19:57:01 -0700

    style site header variants

--- themes/coop-radio/header.php
+++ themes/coop-radio/header.php
@@ -20,25 +20,44 @@
     <div class="hfeed">
       <a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
 
-      <header class='banner' role="banner">
+      <header
+        class="banner <?php 
+          if ( is_page_template('page-get-involved.php') || is_front_page() ) {
+            echo 'banner--dark ';
+          }
+
+          if ( is_page_template('page-get-involved.php') ) {
+            echo 'banner--desktop-transparent ';
+          } else if (
+            is_page_template('page-artist-search.php')
+              || is_page_template('page-contact-us.php')
+          ) {
+            echo 'banner--desktop-light ';
+          }
+        ?>"
+        role="banner"
+      >
         <div>
-          <h1 class="screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+          <h1 class="screen-reader-text">
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+              <?php bloginfo( 'name' ); ?>
+            </a>
+          </h1>
         </div>
-        <div class='header-container'>
-          <div class='logo-container'>
-            <a class='logo-link' href="<?php echo esc_url(home_url('/')); ?>">
+        <div class="banner__container">
+          <div class="banner__logo-container">
+            <a class="banner__logo-link" href="<?php echo esc_url(home_url('/')); ?>">
               <img src="<?php echo get_stylesheet_directory_uri().'/images/co-op logo-dark.png'; ?>" alt="co-op radio logo">
             </a>
           </div>
           <nav id="site-navigation" class="main-navigation" role="navigation">
             <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"></button>
             <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-
           </nav>
-          <div class='header-search'>
-						<?php get_search_form(); ?>
+          <div class="header-search">
+            <?php get_search_form(); ?>
           </div>
-          <div class='search-icon'>
+          <div class="search-icon">
             <a href="">
               <img src="<?php echo get_stylesheet_directory_uri().'/images/search-icon-white.svg'; ?>" alt="search icon">
             </a>

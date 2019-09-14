0fb62a29d841520a577caa521b4d73aa28ec79e3
fyeung21 <fyeung21@gmail.com>
2019-09-14 14:32:20 -0700

    make clickable permalink area include artist image too

--- themes/coop-radio/template-parts/content-artist-library.php
+++ themes/coop-radio/template-parts/content-artist-library.php
@@ -3,27 +3,23 @@
  * Template part for single artist in artist library.
  *
  * @package coop-radio
- */
-
+ */ 
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-  <?php if ( has_post_thumbnail() ) : ?>
+  <a href="<?= esc_url( get_post_permalink( get_the_ID() ) ); ?>"
+      rel="bookmark">
+
+    <?php if ( has_post_thumbnail() ) : ?>
     <?php the_post_thumbnail( 'large' ); ?>
-  <?php endif; ?>
+    <?php endif; ?>
 
-  <h2>
-    <a
-      href="<?= esc_url( get_post_permalink( get_the_ID() ) ); ?>"
-      rel="bookmark"
-    >
-      <?= the_title(); ?>
-    </a>
-  </h2>
+    <h2><?= the_title(); ?></h2>
+  
+  </a>
 
-  <?php
-    $terms = get_the_terms( get_the_ID(), 'genre'  );
+  <?php $terms = get_the_terms( get_the_ID(), 'genre'  );
 
       if ( gettype( $terms ) == 'array' ) : 
         foreach ( $terms as $term  ) : ?>
@@ -34,7 +30,6 @@
 
           <p>Misc</p>
 
-        <?php endif;
-  ?>
+        <?php endif; ?>
 
 </article>

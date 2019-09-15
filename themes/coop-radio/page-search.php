0f24e7e9d32934a40201f2df78faafbddcb74f4a
Hongsoon Kim <khsun6226@naver.com>
2019-09-14 17:10:42 -0700

    add searchform to custom search page

--- themes/coop-radio/page-search.php
+++ themes/coop-radio/page-search.php
@@ -9,11 +9,19 @@ get_header(); ?>
 
   <main>
 
-    <?php while ( have_posts() ) : the_post(); ?>
-
-      <?php get_template_part( 'template-parts/content', 'search' ); ?>
-
-    <?php endwhile; // End of the loop. ?>
+    <div class='custom-search'>
+      <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+	      <fieldset>
+		      <button type='button' class="search-toggle" aria-hidden="true">
+            <img src="<?php echo get_stylesheet_directory_uri().'/images/search-icon-white.svg'; ?>" alt="">
+	        </button>
+		      <span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
+		      <label class='search-label'>
+			      <input type="search" class="search-field" placeholder="Type and hit enter..." value="<?php the_search_query(); ?>" name="s" title="Search for:" />
+		      </label>
+	      </fieldset>
+      </form>
+    </div>
 
   </main>
 

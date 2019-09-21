3e0ff3d9bd8102329dc90a38b87f40e4833ec88f
fyeung21 <fyeung21@gmail.com>
2019-09-21 00:31:41 -0700

    style enroll desktop ver

--- themes/coop-radio/template-parts/content-page-program-details__enroll.php
+++ themes/coop-radio/template-parts/content-page-program-details__enroll.php
@@ -10,13 +10,17 @@
 
 <section class="enroll">
   <h2><?= CFS()->get('enroll_title'); ?></h2>
+  <p class="enroll-descript-desk"><?= CFS()->get('enroll_description'); ?></p>
 
-  <div class="enroll-img">
-    <img src="<?= CFS()->get('enroll_img'); ?>">
-  </div>
+  <div class="enroll-main">
+    <div class="enroll-img">
+      <img src="<?= CFS()->get('enroll_img'); ?>">
+    </div>
 
-  <div class="enroll-text">
-    <p><?= CFS()->get('enroll_description'); ?></p>
-    <div class="enroll-input"><?php the_content() ?></div>
+    <div class="enroll-text">
+      <p class="enroll-descript-mob"><?= CFS()->get('enroll_description'); ?></p>
+      <div class="enroll-input"><?php the_content() ?></div>
+    </div>
   </div>
+  
 </section>

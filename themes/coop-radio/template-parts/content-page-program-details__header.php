afedefc8b801c7e91f2f3d67d0eb2adde2e0a974
fyeung21 <fyeung21@gmail.com>
2019-09-21 16:06:00 -0700

    edit program header mobile

--- themes/coop-radio/template-parts/content-page-program-details__header.php
+++ themes/coop-radio/template-parts/content-page-program-details__header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for program details page header.
  *
@@ -7,34 +8,29 @@
 
 ?>
 
-<header
-  class="program-details__header"
-  style="background-image: url(
-    <?= wp_get_attachment_url( get_post_thumbnail_id( $post->ID )); ?>
-  );"
->
-  <h1>
-    <?= CFS()->get( 'program_title' ); ?>
-  </h1>
+<header class="program-details__header" style="background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(
+    <?= wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
+  );">
+  <article class="program-header-info">
+    <div class="program-header-text">
+      <h3 class="program-title">
+        <?= CFS()->get('program_title'); ?>
+      </h3>
 
-  <h2>
-    <?= CFS()->get( 'featured_content_title' ); ?>
-  </h2>
+      <h2 class="featured-title">
+        <?= CFS()->get('featured_content_title'); ?>
+      </h2>
 
-  <p>
-    <?= CFS()->get( 'featured_content_subtitle' ); ?>
-  </p>
+      <h1 class="featured-subtitle">
+        <?= CFS()->get('featured_content_subtitle'); ?>
+      </h1>
+    </div>
 
-  <div class="program-details__actions">
-    <button
-      class="btn-medium btn-primary program-details__action"
-      type="button"
-    >
-      Sign up
-    </button>
+    <div class="program-details__actions">
+      <button class="btn-medium btn-primary program-details__action" type="button">
+        Sign up
+      </button>
+      <a href="/">Watch the film</a>
 
-    <a href="/">
-      Watch the film
-    </a>
-  </div>
+  </article>
 </header>

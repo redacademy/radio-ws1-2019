acaefed3b5f0c676698eea8c007fa89519b17e6e
William <w-lindvall@outlook.com>
2019-09-21 15:02:50 -0700

    inline search page bg img

--- themes/coop-radio/page-search.php
+++ themes/coop-radio/page-search.php
@@ -7,22 +7,25 @@
 
 get_header(); ?>
 
-  <main>
-	<div class='search-background'>
-		<div class='search-intro'>
-			<p>Looking for something?</p>
-		</div>
-		<div class='custom-search'>
-			<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-				<fieldset>
-					<span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
-					<label class='custom-search-label'>
-						<input type="search" class="custom-input" placeholder="Type and hit enter..." value="<?php the_search_query(); ?>" name="s" title="Search for:" />
-					</label>
-				</fieldset>
-			</form>
-		</div>
-	</div>
-  </main>
+<main>
+  <div
+    class="search-container"
+    style="background-image: url('<?= get_stylesheet_directory_uri().'/images/search-background.jpg'; ?>'); "
+  >
+    <div class="search-intro">
+      <p>Looking for something?</p>
+    </div>
+    <div class="custom-search">
+      <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+        <fieldset>
+          <span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
+          <label class="custom-search-label">
+            <input type="search" class="custom-input" placeholder="Type and hit enter..." value="<?php the_search_query(); ?>" name="s" title="Search for:" />
+          </label>
+        </fieldset>
+      </form>
+    </div>
+  </div>
+</main>
 
 <?php get_footer(); ?>

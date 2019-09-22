0638f0dd03b76e04ff21dda52cd7cdb864ddeecd
William <w-lindvall@outlook.com>
2019-09-21 22:29:07 -0700

    style full width program details page yt embed

--- themes/coop-radio/template-parts/content-page-program-details__video.php
+++ themes/coop-radio/template-parts/content-page-program-details__video.php
@@ -8,8 +8,9 @@
 ?>
 
 <section>
-  <div class="text-container">
+  <div class="text-container program-details__video-container">
     <iframe
+      class="program-details__video-embed"
       width="560"
       height="315"
       src="https://www.youtube.com/embed/<?= CFS()->get( 'featured_video_id' ); ?>"

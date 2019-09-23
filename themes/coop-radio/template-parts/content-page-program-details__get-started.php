571a5ba3664e6b4bd011c5f24305af15d84923a1
fyeung21 <fyeung21@gmail.com>
2019-09-22 23:43:56 -0700

    edit and apply wide screen mixin to get started section

--- themes/coop-radio/template-parts/content-page-program-details__get-started.php
+++ themes/coop-radio/template-parts/content-page-program-details__get-started.php
@@ -16,22 +16,22 @@ if (isset($sections) && sizeof($sections) > 0) : ?>
     <h2><?= CFS()->get('how_to_title'); ?></h2>
 
     <div class="get-started-main">
-    <?php foreach ($sections as $section) { ?>
+      <?php foreach ($sections as $section) { ?>
 
-      <div class="get-started-item">
-        <div class="get-started-img">
-          <img src="<?= $section['icon']; ?>" alt="" />
-        </div>
+        <div class="get-started-item">
+          <div class="get-started-img">
+            <img src="<?= $section['icon']; ?>" alt="" />
+          </div>
 
-        <div class="get-started-text">
-          <h4><?= $section['how_to_step_num']; ?></h4>
-          <h3><?= $section['title']; ?></h3>
-          <p><?= $section['description']; ?></p>
-        </div>
-      </div>
+          <div class="get-started-text">
+            <h4><?= $section['how_to_step_num']; ?></h4>
+            <h3><?= $section['title']; ?></h3>
+            <p><?= $section['description']; ?></p>
+          </div>
+        </div><!-- get-started-item -->
 
-    <?php } ?>
-    </div>
+      <?php } ?><!-- end foreach -->
+    </div><!-- get-started-main -->
   </section>
 
 <?php endif; ?>

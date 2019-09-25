8e7d15e13d98c446b67687c5cdda89f96ace3d72
graziela silva <grazims26@gmail.com>
2019-09-24 20:23:41 -0700

    add class to donate page

--- themes/coop-radio/inc/extras.php
+++ themes/coop-radio/inc/extras.php
@@ -25,7 +25,9 @@ function coop_radio_body_classes( $classes ) {
   if ( is_page('contact-us')) {
 		$classes[] = 'contact-us-page';
   }
-  
+  if ( is_page('donate')) {
+		$classes[] = 'donate-page';
+  }
   return $classes;
 }
 add_filter( 'body_class', 'coop_radio_body_classes' );

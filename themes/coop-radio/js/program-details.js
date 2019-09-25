aaaf82aafd0d3c607ebcc95a8a7d321ee63ded67
fyeung21 <fyeung21@gmail.com>
2019-09-24 17:41:06 -0700

    delete extra console logs

--- themes/coop-radio/js/program-details.js
+++ themes/coop-radio/js/program-details.js
@@ -2,10 +2,8 @@
 
 const enrollNow = document.getElementsByClassName('enroll-now')[0];
 
-console.log(enrollNow, 'enroll button');
 enrollNow.addEventListener("click", function (event) {
     event.preventDefault();
-    console.log(event.target);
     document.getElementById(`${event.target.dataset.target}`).scrollIntoView({ behavior: 'smooth' })
 });
 

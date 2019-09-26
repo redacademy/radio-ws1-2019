48c7b420e3babf67969aef11b851f380dd4ef98a
fyeung21 <fyeung21@gmail.com>
2019-09-26 01:04:02 -0700

    make back top icon appear on scroll

--- themes/coop-radio/js/headerScroll.js
+++ themes/coop-radio/js/headerScroll.js
@@ -7,6 +7,7 @@
 jQuery(function() {
     let header = jQuery('.banner--transparent');
     let search = jQuery('.search-icon-black');
+    let backTopIcon = jQuery('.back-top');
     let main = jQuery('main');
     
     main.scroll(function() {
@@ -14,9 +15,11 @@ jQuery(function() {
         if (scroll >= 1) {
             header.removeClass('banner--transparent').addClass('banner--light');
             search.removeClass('search-icon-black').addClass('search-icon-white');
+            backTopIcon.addClass('back-top');
         } else {
             header.removeClass('banner--light').addClass('banner--transparent');
             search.removeClass('search-icon-white').addClass('search-icon-black');
+            backTopIcon.removeClass();
         }
     })
 })
